4bb21f7a9c3b00d2c47c6deaeff4d92e4ebddd0b
eduardomarquesgonzalez <eduardomarquesgonzalez@gmail.com>
2021-10-15 10:07:15 -0400

    angular material adicionado ao projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -12,6 +13,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
    
   ],
   providers: [],

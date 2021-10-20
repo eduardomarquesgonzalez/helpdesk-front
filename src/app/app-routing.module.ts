337b6bc20d2dd70ac4c99bdafd94d47933d3eb87
eduardomarquesgonzalez <eduardomarquesgonzalez@gmail.com>
2021-10-20 09:07:28 -0400

    rota para TECNICO-LIST criado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { NavigationStart, RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavComponent } from './components/nav/nav.component';
+import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 
 const routes: Routes = [
  
   {
     path: '', component: NavComponent,  children: [
       { path: 'home', component: HomeComponent },
+      {path:'tecnicos',component: TecnicoListComponent}
     ]}
     
 ];

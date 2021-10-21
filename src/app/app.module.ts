e08b31cc6c2878dbe24aaabcda69a869bd458d97
eduardomarquesgonzalez <eduardomarquesgonzalez@gmail.com>
2021-10-20 21:52:54 -0400

    serviço de mensagens implementado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -33,6 +33,7 @@ import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 import { LoginComponent } from './components/login/login.component';
+import { ToastrModule } from 'ngx-toastr';
 
 
 
@@ -70,7 +71,12 @@ import { LoginComponent } from './components/login/login.component';
     MatIconModule,
     MatListModule,
     MatCardModule,
-   
+    ToastrModule.forRoot({
+      timeOut: 4000,
+      closeButton: true,
+      progressBar: true
+    })
+       
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 import { Credenciais } from 'src/app/models/credenciais';
 
 @Component({
@@ -8,7 +9,7 @@ import { Credenciais } from 'src/app/models/credenciais';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  //para validar preenchimento dos campos de login
   creds: Credenciais = {
     email:'',
     senha:''
@@ -16,12 +17,17 @@ export class LoginComponent implements OnInit {
 
   email = new FormControl(null, Validators.email);
   senha = new FormControl(null, Validators.minLength(3))
-
-  constructor() { }
+  //insere no constructor para utilizar o toastr no metodo (logar)
+  constructor(private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
-
+  // para exibir um modal 
+  logar (){
+    this.toastr.error('usuario e/ou senha invalidos', 'Login');
+    this.creds.senha = '';
+    
+  }
   validaCampos(): boolean {
     if(this.email.valid && this.senha.valid){
       return true;

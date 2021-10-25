b84137335625280fb5a2b2a724edc4c010622c39
eduardomarquesgonzalez <eduardomarquesgonzalez@gmail.com>
2021-10-25 15:28:20 -0400

    validação dos inputs do componente TECNICO-CREATE

--- src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
+++ src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-tecnico-create',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TecnicoCreateComponent implements OnInit {
 
+  
+  nome = new FormControl(null, Validators.minLength(3))
+  cpf = new FormControl(null, Validators.required)
+  email = new FormControl(null, Validators.email)
+  senha = new FormControl(null, Validators.minLength(3))
+ 
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  //metodo para validar campo retornando um boolean
+  validCampos(): boolean{
+    return this.nome.valid && 
+            this.cpf.valid  && 
+            this.email.valid && 
+            this.senha.valid
   }
 
 }
